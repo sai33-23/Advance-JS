d6e387e30ad062b3f4d22c9cec38ad72b0042c14
Sai Jyotirmayee <84378922+sai33-23@users.noreply.github.com>
2022-01-22 19:32:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// regular function
+
+// function sai(){
+//     console.log("Hey this is sai")
+// // }
+// sai();
+
+// const sai = function(){
+//     console.log("Hey this is sai")
+// }
+// sai();
+
+// converting in to arraw function
+
+
+// const sai = ()=>{
+//     console.log("Hey this is sai")
+// }
+// sai();
+
+// const smile = ()=>{
+//     return "open with smile"
+// }
+// console.log(smile())
+
+// const smile = ()=>"open with smile";
+// console.log(smile())
+
+// const me = ()=> ({name:"sai"});
+// console.log(me())
+
+const greet = (name,ending) => "Good morning " + name + ending;
+console.log(greet("Sai ","bye"))
+
+// if we are taking more than two arguments then we have to give paranthesis 
+// in the function otherwise it will give error
