f5eb2f86f6b58549beb147e72a9af923e4b0365d
Sai Jyotirmayee <84378922+sai33-23@users.noreply.github.com>
2022-01-22 19:33:39 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+// function Alien(name,tech){
+//     this.name = name;
+//     this.tech = tech;
+//     this.work = function(){
+//         console.log("solving the problems")
+    // }
+// }
+// let alien1 = new Alien("sai","js");
+// let alien2 = new Alien("siddharth","python");
+// alien1.tech = "Blockchain";
+// console.log(alien);
+
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+
